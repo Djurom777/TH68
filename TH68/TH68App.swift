eb1aa99be25658a1cb439633e34c347215350236
DJUROM777 <djurom777@ya.ru>
2025-09-19 16:40:48 +0300

    Initial Commit

---
+++ TH68/TH68App.swift
@@ -0,0 +1,17 @@
+//
+//  TH68App.swift
+//  TH68
+//
+//  Created by IGOR on 19/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct TH68App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TH68Tests/TH68Tests.swift
@@ -0,0 +1,16 @@
+//
+//  TH68Tests.swift
+//  TH68Tests
+//
+//  Created by IGOR on 19/09/2025.
+//
+
+import Testing
+
+struct TH68Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
